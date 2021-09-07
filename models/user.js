3c2c5a521686ef0ad4844d2c82c5db66c4a9e075
xuan1106 <spu96508@gmail.com>
2021-09-07 11:34:04 +0800

    Create followship model and add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -12,15 +12,25 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       User.hasMany(models.Comment)
       User.belongsToMany(models.Restaurant, {
-      through: models.Favorite,
-      foreignKey: 'UserId',
-      as: 'FavoritedRestaurants'
-    })
+        through: models.Favorite,
+        foreignKey: 'UserId',
+        as: 'FavoritedRestaurants'
+      })
       User.belongsToMany(models.Restaurant, {
         through: models.Like,
         foreignKey: 'UserId',
         as: 'LikedRestaurants'
       })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+     })
     }
   };
   User.init({
